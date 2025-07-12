10d5c51da41e97f3f7f31a6dd1de017388320311
Djalma Xavier <djalmaxavier42@gmail.com>
2025-07-11 23:43:21 -0300

    Feat(user): Add entity and repository

---
+++ backend/src/main/java/com/dx/expense/entities/User.java
@@ -0,0 +1,68 @@
+package com.dx.expense.entities;
+
+import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Set;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Getter
+@NoArgsConstructor
+@EqualsAndHashCode
+@Entity
+@Table(name = "tb_user")
+public class User implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    private String name;
+
+    @NotNull
+    @Column(unique = true)
+    private String login;
+
+    @NotNull
+    private String password;
+
+    private double salary;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    @ToString.Exclude
+    @JsonIgnore
+    private Set<Expenses> expenses = new HashSet<>();
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+}
