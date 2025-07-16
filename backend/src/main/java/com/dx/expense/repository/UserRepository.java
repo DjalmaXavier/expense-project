22358e284c40ca4497d09767838c4e21877db6c2
Djalma Xavier <djalmaxavier@gmail.com>
2025-07-15 23:33:15 -0300

    Feat(UserRepository(: add findByLogin method

--- backend/src/main/java/com/dx/expense/repository/UserRepository.java
+++ backend/src/main/java/com/dx/expense/repository/UserRepository.java
@@ -1,9 +1,11 @@
 package com.dx.expense.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.dx.expense.entities.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-
+    Optional<User> findByLogin(String login);
 }
