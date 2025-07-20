92dfc85be02f1b861874e9b87872d87297f4baa5
Djalma Xavier <djalmaxavier@gmail.com>
2025-07-19 23:51:38 -0300

    Feat(Auth): Add resource

---
+++ backend/src/main/java/com/dx/expense/controllers/AuthResource.java
@@ -0,0 +1,10 @@
+package com.dx.expense.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/auth")
+public class AuthResource {
+
+}
