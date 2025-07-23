3d3f8ee843bfae5b795a277ca027bd729c9af4c0
Djalma Xavier <djalmaxavier42@gmail.com>
2025-07-22 21:58:32 -0300

    Feat(Repository): Add optional methods to find roles and users

--- backend/src/main/java/com/dx/expense/repository/RoleRepository.java
+++ backend/src/main/java/com/dx/expense/repository/RoleRepository.java
@@ -1,9 +1,11 @@
 package com.dx.expense.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.dx.expense.entities.Role;
 
 public interface RoleRepository extends JpaRepository<Role, Long> {
-
+    Optional<Role> findByDescription(String description);
 }
