387a982c7c96172e850173148f0d0d6b6f9e3d19
Djalma Xavier <djalmaxavier42@gmail.com>
2025-07-28 20:12:22 -0300

    feat(AuthResource): Add register and login endpoints

--- backend/src/main/java/com/dx/expense/controllers/AuthResource.java
+++ backend/src/main/java/com/dx/expense/controllers/AuthResource.java
@@ -3,8 +3,57 @@ package com.dx.expense.controllers;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.dx.expense.dto.LoginRequestDTO;
+import com.dx.expense.dto.LoginResponseDTO;
+import com.dx.expense.dto.RegisterDTO;
+import com.dx.expense.services.JwtTokenService;
+import com.dx.expense.services.UserServices;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
 @RestController
 @RequestMapping(value = "/auth")
 public class AuthResource {
+
+    private final UserServices userServices;
+
+    private final JwtTokenService tokenService;
+
+    @Autowired
+    public AuthResource(UserServices userServices, JwtTokenService tokenService) {
+        this.userServices = userServices;
+        this.tokenService = tokenService;
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<String> register(@RequestBody RegisterDTO registerDTO) {
+        try {
+            userServices.registerUser(registerDTO);
+            return ResponseEntity.status(HttpStatus.CREATED).build();
+        } catch (RuntimeException e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+        }
+
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<?> login(@RequestBody LoginRequestDTO loginRequestDTO) {
+        try {
+            var user = userServices.loginResponse(loginRequestDTO);
+
+            var token = tokenService.generateToken(user.getLogin());
+
+            return ResponseEntity.ok(new LoginResponseDTO(token));
+        } catch (BadCredentialsException e) {
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).body("Credenciais inválidas: " + e.getMessage());
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Erro interno: " + e.getMessage());
+        }
+    }
 
 }
