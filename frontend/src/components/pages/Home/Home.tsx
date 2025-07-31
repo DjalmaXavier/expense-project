02d206cfaebe05c9996e6b869b9ae243258daa6d
Djalma Xavier <djalmaxavier42@gmail.com>
2025-07-30 21:15:10 -0300

    feat(pages): add styles import into NavBar and Home

---
+++ frontend/src/components/pages/Home/Home.tsx
@@ -0,0 +1,12 @@
+import NavBar from "../../layout/NavBar";
+import styles from "./styles.module.css";
+
+function Home() {
+  return (
+    <>
+      <NavBar />
+    </>
+  );
+}
+
+export default Home;
