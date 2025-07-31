7d7ce78a2862361c8c8068e3c7777bab8fb82ef3
Djalma Xavier <djalmaxavier42@gmail.com>
2025-07-30 21:23:46 -0300

    refactor(test): remove old test, working on

--- backend/src/test/java/com/dx/expense/services/UserServicesTest.java
+++ backend/src/test/java/com/dx/expense/services/UserServicesTest.java
@@ -1,19 +1,12 @@
 package com.dx.expense.services;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
-import java.util.Optional;
-
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
-import com.dx.expense.dto.LoginDTO;
-import com.dx.expense.dto.RegisterDTO;
-import com.dx.expense.entities.User;
 import com.dx.expense.repository.UserRepository;
 
 @ExtendWith(MockitoExtension.class)
@@ -25,29 +18,17 @@ public class UserServicesTest {
     @InjectMocks
     private UserServices userService;
 
+    @Mock
+    BCryptPasswordEncoder bCryptPasswordEncoder;
+
     @Test
     void mustRegisterUser() {
 
-        RegisterDTO register = new RegisterDTO("teste@teste", "1234567", "teste");
-
-        userService.registeredUser(register);
-
+        // Working...
     }
 
     @Test
     void mustLoginUser() {
 
-        // Arrange
-        LoginDTO login = new LoginDTO("teste@teste", "1234567");
-
-        User user = new User("teste", "teste@teste", "1234567");
-
-        Mockito.when(userRepository.findByLogin(login.email())).thenReturn(Optional.of(user));
-
-        // Act
-        User test = userService.loginResponse(login);
-
-        // Asserty
-        assertEquals(user.getLogin(), test.getLogin());
     }
 }
