537556826990f1c4a5cf9e930401bc0dded7540a
Djalma Xavier <djalmaxavier42@proton.me>
2025-10-18 09:08:04 -0300

    Add(Route): Login route

--- frontend/src/routes/AppRoutes.tsx
+++ frontend/src/routes/AppRoutes.tsx
@@ -1,12 +1,11 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { Home, Login, Register } from "../components/pages/indexPages";
+import { Home, Register } from "../components/pages/indexPages";
 
 export default function AppRoutes() {
   return (
     <BrowserRouter>
       <Routes>
         <Route index element={<Home />} />
-        <Route path="login" element={<Login />} />
         <Route path="register" element={<Register />} />
       </Routes>
     </BrowserRouter>
