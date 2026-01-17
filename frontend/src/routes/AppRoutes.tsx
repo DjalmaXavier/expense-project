dabef6bf73a22b60f9309bb2b191bbcc43f9c657
Djalma Xavier <djalmaxavier42@proton.me>
2026-01-17 00:57:09 -0300

    Add(Routes): Add new routes

--- frontend/src/routes/AppRoutes.tsx
+++ frontend/src/routes/AppRoutes.tsx
@@ -1,12 +1,27 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { Home, Register } from "@src/components/pages/indexPages";
+import {
+  Home,
+  Register,
+  Dashboard,
+  PageNotFound,
+} from "@src/components/pages/indexPages";
+import RequireAuth from "./RequireAuth";
 
 export default function AppRoutes() {
   return (
     <BrowserRouter>
       <Routes>
         <Route index element={<Home />} />
+        <Route path="*" element={<PageNotFound />} />
         <Route path="register" element={<Register />} />
+        <Route
+          path="/dashboard"
+          element={
+            <RequireAuth>
+              <Dashboard />
+            </RequireAuth>
+          }
+        />
       </Routes>
     </BrowserRouter>
   );
