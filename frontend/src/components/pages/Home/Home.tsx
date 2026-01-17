47f7395afc1db9ddbd1c611cc46f3940694a5e0e
Djalma Xavier <djalmaxavier42@proton.me>
2026-01-17 01:01:47 -0300

    Add and Update(Pages): - Add flashcard - Updated styles and login Button

--- frontend/src/components/pages/Home/Home.tsx
+++ frontend/src/components/pages/Home/Home.tsx
@@ -1,57 +1,69 @@
 import { useState } from "react";
-import FootBar from "../../layout/FootBar";
-import NavBar from "../../layout/NavBar";
-import styles from "./styles.module.css";
-import { loginUser } from "../../../api/ApiLogin";
 import { Link, useNavigate } from "react-router-dom";
+import { useAuth } from "@hook/useAuth";
+import { FootBar, NavBar } from "@src/components/layout/indexLayouts";
+import { FlashMessageError } from "@src/components/utils/FlashMessages";
+import styles from "./styles.module.css";
 
 function Home() {
   const navigator = useNavigate();
-  const [loginAuth, setLoginAuth] = useState("");
+  const { login } = useAuth();
+  const [loginAuth, setLoginAuth] = useState<string | null>(null);
   const [password, setPassword] = useState("");
+  const [flashMessage, setFlashMessage] = useState<string | null>(null);
 
   const loginButton = async (e: any) => {
     e.preventDefault();
-    const user = { email: loginAuth, password };
-    await loginUser(user).then((data) => {
-      navigator("/main");
-      console.log(data);
-    });
+    const check = await login(loginAuth, password);
+    if (!check) {
+      setFlashMessage("Usuário e/ou senha incorretos!");
+    } else {
+      navigator("/dashboard");
+    }
   };
   return (
     <>
-      <NavBar />
-      <main className={styles.main}>
-        <div className={styles.content}>
-          <h1>Controle de gastos</h1>
-          <form className={styles.form}>
-            <span className={styles.input_span}>
-              <label className={styles.label}>Email</label>
+      <div className="app">
+        <NavBar />
+        <main className={styles.main}>
+          <div className={styles.content}>
+            <h1>Controle de gastos</h1>
+            <form className={styles.form}>
+              <span className={styles.input_span}>
+                <label className={styles.label}>Email</label>
+                <input
+                  type="email"
+                  onChange={(e: any) => setLoginAuth(e.target.value)}
+                />
+              </span>
+              <span className={styles.input_span}>
+                <label className={styles.label}>Password</label>
+                <input
+                  type="password"
+                  onChange={(e: any) => setPassword(e.target.value)}
+                />
+              </span>
               <input
-                type="email"
-                onChange={(e: any) => setLoginAuth(e.target.value)}
+                type="submit"
+                value="Log in"
+                className={styles.submit}
+                onClick={loginButton}
               />
-            </span>
-            <span className={styles.input_span}>
-              <label className={styles.label}>Password</label>
-              <input
-                type="password"
-                onChange={(e: any) => setPassword(e.target.value)}
-              />
-            </span>
-            <input
-              type="submit"
-              value="Log in"
-              className={styles.submit}
-              onClick={loginButton}
-            />
-            <span className={styles.span}>
-              Don't have an account? <Link to={"/register"}>Sign up</Link>
-            </span>
-          </form>
-        </div>
-      </main>
-      <FootBar />
+              <span className={styles.span}>
+                Don't have an account? <Link to={"/register"}>Sign up</Link>
+              </span>
+              {flashMessage && (
+                <FlashMessageError
+                  key={flashMessage}
+                  message={flashMessage}
+                  onClose={() => setFlashMessage(null)}
+                />
+              )}
+            </form>
+          </div>
+        </main>
+        <FootBar />
+      </div>
     </>
   );
 }
